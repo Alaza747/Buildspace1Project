1a8bd44be0f66dcd43b7402d3d454b4f34753c57
Anton Laza <anton.laza.0116@gmail.com>
2022-04-21 20:11:04 +0200

    Added event, struct, array of structs and changed functions

--- Blockchain/my-wave-portal/contracts/WavePortal.sol
+++ Blockchain/my-wave-portal/contracts/WavePortal.sol
@@ -3,24 +3,42 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
+
 // Address : 0x14a5d8b2C5D56F77A7ea2E8Aef5A890DCF932A7f 
+
 contract WavePortal {
     uint256 totalWaves;
-    mapping(address => uint32) public waveByPerson;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
 
-    function wave() public {
+// Struct is a custom data type, where one can define what should be saved
+    struct Wave {
+        address waver; //address of the user who waved
+        string message; // message as string type
+        uint256 timestamp; //timestamp of the wave
+    }
+
+// Declaration of the array of structs, to save all the waves
+    Wave[] waves;
+
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved", msg.sender);
-        waveByPerson[msg.sender] += 1;
+        console.log("%s has waved", msg.sender, _message);
+
+// Storing the wave to the "all-waves" array
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+// With this func it will be easier to retrieve waves 
+    function getAllWaves() public view returns (Wave[] memory){
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256){
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
-    }
-
-    function returnWaveCount() public view returns (uint32) {
-        return waveByPerson[msg.sender];
     }
 }
