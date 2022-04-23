c8ce1baf41393541f4e489e7740ea321f6df16ce
Anton Laza <anton.laza.0116@gmail.com>
2022-04-23 23:05:31 +0200

    env implementation

--- Blockchain/my-wave-portal/hardhat.config.js
+++ Blockchain/my-wave-portal/hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/vo2N2ne5nmkQxdyJufTbl3gqeKZ5V6W8",
-      accounts: ["2f985a18f074432ed09a2aecd355af8ba2eacc994a180a60dcd925e73cafec41"]
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY]
     },
   },
 };
