90157702a2678004849c1c351a63069325a9bead
Alaza747 <83471174+Alaza747@users.noreply.github.com>
2022-04-23 23:47:48 +0200

    Delete Javascript_LinkedLearning directory

--- Javascript_LinkedLearning/Exercise Files/02_04/script.js
+++
@@ -1,8 +0,0 @@
-function findBiggest(){
-  a>b ? console.log("the biggest number is a: ", a) : console.log("the biggest number is b: ", b)
-}
-
-var a = 4/5;
-var b = 7/4;
-
-findBiggest();
--- Javascript_LinkedLearning/Exercise Files/10_03/JS/script.js
+++
@@ -1,6 +0,0 @@
-const IMAGES = document.querySelectorAll("img");
-
-for (let i = 0; i<IMAGES.length; i++){
-  let ImgSrc = IMAGES[i].getAttribute("src");
-  console.log(ImgSrc);
-}
